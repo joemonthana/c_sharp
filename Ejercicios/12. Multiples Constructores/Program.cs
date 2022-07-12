066cb0d5bc57912f83e99ae45a92278ee475f7c9
Joel Torres <montana12@outlook.es>
2022-07-11 19:41:26 -0600

    Agregando multiples constructores

---
+++ Ejercicios/12. Multiples Constructores/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Alumno a = new Alumno();
+a.PrimerNombre = "Juan";
+a.SegundoNombre = "Andres";
+
+Alumno b = new Alumno();
+b.Id = 2;
+b.PrimerNombre = "Maria";
+b.SegundoNombre = "Jose";
+
+Alumno c = new Alumno(3);
+c.PrimerNombre = "Joel";
+
+Alumno d = new Alumno("Mario", "Torres");
+
+Console.WriteLine(a.Id);
+Console.WriteLine(b.Id);
+Console.WriteLine(c.Id);
+Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
